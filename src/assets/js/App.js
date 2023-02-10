c0b4c5457bba6eea9969943370ca83906ef0e152
Poylar <doalesk8@gmail.com>
2023-02-11 01:18:59 +0800

    deploy

--- src/assets/js/App.js
+++
@@ -1,5 +0,0 @@
-import './Init';
-import './Video';
-import './AttachedFiles';
-import './Scrolling';
-import './Archon';
--- src/index.js
+++
@@ -1,4 +0,0 @@
-import './styles/App.scss';
-import './assets/js/App';
-
-require.context('./assets/icons', true, /\.svg$/);
