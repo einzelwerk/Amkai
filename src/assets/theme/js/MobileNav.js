01c1d397ec9023fbe7025391d45588f95a1ecee9
clutch4t <clutch4t@gmail.com>
2023-08-03 20:37:22 +0500

    deploy

--- src/assets/theme/js/MobileNav.js
+++
@@ -1,7 +0,0 @@
-const mobileNavBtn = document.querySelectorAll('.mobile-nav__arrow');
-mobileNavBtn.forEach((elem) => {
-  elem.addEventListener('click', (e) => {
-    const target = e.currentTarget.closest('.mobile-nav__item').querySelector('.mobile-sub');
-    target.classList.toggle('opened');
-  });
-});
--- src/index.js
+++
@@ -1,4 +0,0 @@
-import './assets/theme/js/App';
-import './styles/App.scss';
-
-require.context('@/icons', true, /\.svg$/);
