f48c4be870b67adde001657bf218ee4991dcfe27
clutch4t <clutch4t@gmail.com>
2023-08-22 17:00:45 +0500

    Deploy video player changes

--- src/assets/theme/js/ReelsVideoPlayer.js
+++ src/assets/theme/js/ReelsVideoPlayer.js
@@ -1,27 +1,25 @@
 const playButton = document.querySelectorAll('.reels__video-playBtn');
-window.addEventListener('load', () => {
-  playButton.forEach((el) => {
-    const elem = el;
-    elem.addEventListener('click', () => {
-      const video = elem.previousElementSibling;
-      if (!video.playing) {
-        video.play();
-        video.setAttribute('controls', true);
-        elem.style.display = 'none';
-      }
+playButton.forEach((el) => {
+  const elem = el;
+  elem.addEventListener('click', () => {
+    const video = elem.closest('.swiper-slide').querySelector('video');
+    if (!video.playing) {
+      video.play();
+      video.setAttribute('controls', true);
+      elem.style.display = 'none';
+    }
 
-      video.addEventListener('mouseenter', () => {
-        video.play();
-      });
-      video.addEventListener('pointerenter', () => {
-        video.play();
-      });
-      video.addEventListener('mouseleave', () => {
-        video.pause();
-      });
-      video.addEventListener('pointerleave', () => {
-        video.pause();
-      });
+    video.addEventListener('mouseenter', () => {
+      video.play();
+    });
+    video.addEventListener('mouseleave', () => {
+      video.pause();
+    });
+    video.addEventListener('pointerenter', () => {
+      video.play();
+    });
+    video.addEventListener('pointerleave', () => {
+      video.pause();
     });
   });
 });
