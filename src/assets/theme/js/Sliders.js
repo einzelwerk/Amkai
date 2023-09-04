961d23ab1f73564ea4bc2c4a2295ff4b8617df58
clutch4t <clutch4t@gmail.com>
2023-09-04 19:17:22 +0500

    Restaurant page done

--- src/assets/theme/js/Sliders.js
+++ src/assets/theme/js/Sliders.js
@@ -63,6 +63,29 @@ class Sliders {
     sliderInstance.init();
   }
 
+  static facts() {
+    const root = document.querySelector('.facts-restaurant__slider');
+    const sliderInstance = new Swiper(root, {
+      init: false,
+      slidesPerView: 1.5,
+      spaceBetween: 24,
+      initialSlide: 1,
+      navigation: false,
+      breakpoints: {
+        576: {
+          spaceBetween: 42,
+          slidesPerView: 2,
+        },
+        768: {
+          spaceBetween: 30,
+          slidesPerView: 3,
+        },
+      },
+    });
+
+    sliderInstance.init();
+  }
+
   static reels() {
     const root = document.querySelector('.reels__slider');
     const sliderInstance = new Swiper(root, {
@@ -83,10 +106,32 @@ class Sliders {
     });
     sliderInstance.init();
   }
+
+  static menu() {
+    const root = document.querySelector('.menu-restaurant__slider');
+    const sliderInstance = new Swiper(root, {
+      modules: [Navigation, EffectCards, Pagination],
+      pagination: {
+        el: '.menu-restaurant__numbers',
+        type: 'fraction',
+      },
+      init: false,
+      effect: 'cards',
+      grabCursor: true,
+      navigation: {
+        nextEl: root?.querySelector('.slider-navigation__next'),
+        prevEl: root?.querySelector('.slider-navigation__prev'),
+        disabledClass: 'slider-navigation__btn--disabled',
+      },
+    });
+    sliderInstance.init();
+  }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   Sliders.reviews();
   Sliders.hero();
   Sliders.reels();
+  Sliders.facts();
+  Sliders.menu();
 });
