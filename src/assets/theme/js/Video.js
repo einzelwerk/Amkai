c11ebfb1159f4dbb271dd369b8aac72091cdcc44
Poylar <doaleks8@gmail.com>
2023-11-16 20:33:57 +0800

    deploy

--- src/assets/theme/js/Video.js
+++ src/assets/theme/js/Video.js
@@ -3,7 +3,7 @@ playTrigger.forEach((el) => {
   const elem = el;
   elem.addEventListener('click', () => {
     const video = elem.closest('.video').querySelector('video');
-
+    document?.querySelector('.video-title').remove();
     if (video.paused) {
       video.play();
       video.setAttribute('controls', true);
